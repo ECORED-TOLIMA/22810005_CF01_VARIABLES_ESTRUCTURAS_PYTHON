f9fc4a8aec6857794c5c858d8f2c15694f4374c5
manuelechavarria <maechavarriaor@gmail.com>
2026-07-24 09:36:08 -0500

    intro

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -84,26 +84,14 @@
 
 </template>
 
-
-
 <script>
-
 export default {
-
   name: 'Introduccion',
 
   data: () => ({
-
     // variables de vue
-
   }),
-
 }
-
 </script>
 
-
-
 <style lang="sass"></style>
-
-
